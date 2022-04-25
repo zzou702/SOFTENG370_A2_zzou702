c815c559ce22a02dcc2ac219d44203e90bbc0f79
Zimo Zou <zzou702@aucklanduni.ac.nz>
2022-04-25 21:24:24 +1200

    familiarising with python os

--- sync.py
+++ sync.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
-
+from asyncore import socket_map
+import os
 import sys
 
 dir1 = sys.argv[1]
 dir2 = sys.argv[2]
 
 print(dir1, dir2)
+
+def check_directory():
+    print(os.getcwd())
+    return os.path.exists(os.getcwd())
+
+something = check_directory()
+print(something)
